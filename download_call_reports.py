6b96a198db802fecd8544bb108ac9b5688bf7e60
Andrea Pasqualini <apsql@users.noreply.github.com>
2019-08-23 19:32:44 +0200

    Rename file and add CLI interface

--- download_call_reports.py
+++ download_call_reports.py
@@ -1,25 +1,30 @@
+import sys
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 
-
-driverLocation = "C:/Users/apsql/.chromedriver_win32/chromedriver.exe"
 webLocation = "https://cdr.ffiec.gov/public/PWS/DownloadBulkData.aspx"
 
-browser = webdriver.Chrome(driverLocation)
-browser.get(webLocation)
+try:
+    driverLocation = sys.argv[1]
+    browser = webdriver.Chrome(driverLocation)
+except IndexError:
+    browser = webdriver.Chrome()
+finally:
+    browser.get(webLocation)
 
-# select by id="ListBox1" -> click label="CallReports -- SingleReports"
+# Step 1: Select one from Available Products
 listBox1 = browser.find_element_by_id("ListBox1")
 Select(listBox1).select_by_visible_text("Call Reports -- Single Period")
 
-# select by id="TSVRadioButton" -> click
+# Step 2: Select one of Available File Formats
 browser.find_element_by_id("TSVRadioButton").click()
 
-# list all available dates on dropdown menu
+# Step 3: List all dates on dropdown menu from Reporting Period End Date
 datesDropDownList = browser.find_element_by_id("DatesDropDownList")
 select = Select(datesDropDownList)  # select is the drop-down menu
 availableDates = [date.text for date in select.options]
 
+# Step 4: Click Download button for each available date
 for date in availableDates:
     datesDropDownList = browser.find_element_by_id("DatesDropDownList")
     Select(datesDropDownList).select_by_visible_text(date)
